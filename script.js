91a3914dd6acf7fd8e5cbdfd140bba8bf6550b09
MaksRozhytskyi <rozhik@protonmail.com>
2025-07-13 18:03:04 +0300

    Added burger

--- script.js
+++ script.js
@@ -6,3 +6,14 @@ $(document).ready(function(){
         items: 1
     });
 });
+
+(function() {
+    var burger = document.querySelector('.mobile-menu-container .burger-container'); // Target burger inside mobile menu
+    var mobileMenu = document.querySelector('.mobile-menu-container'); // Target the mobile menu container
+
+    if (burger && mobileMenu) { // Check if elements exist
+        burger.onclick = function() {
+            mobileMenu.classList.toggle('menu-opened');
+        }
+    }
+})();
